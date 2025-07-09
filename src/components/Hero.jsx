700e13cb924f008faa248fb7802bba3636dd3ac6
Coding-master <harshitr2308@gmail.com>
2025-07-09 13:30:15 +0530

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/Harshitt23/Online-Voting-System", "_blank")
           }
           className='black_btn'
         >

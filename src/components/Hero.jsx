65a6abfc4ce9571b506ba2799d04208ba18aaa9b
Coding-master <harshitr2308@gmail.com>
2025-07-10 01:15:01 +0530

    Add enhanced styling system with modern components and animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,11 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain hover:scale-105 transition-transform duration-300' />
+        <img 
+          src={logo} 
+          alt='sumz_logo' 
+          className='w-28 object-contain hover:scale-105 transition-transform duration-300 earth-icon-glow' 
+        />
 
         <button
           type='button'

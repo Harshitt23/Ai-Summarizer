d45d429a7b7daea941dc05501a964a3a4b3bfb8e
Coding-master <harshitr2308@gmail.com>
2025-07-10 00:49:43 +0530

    feat: modernize UI, update fonts, gradients, and GitHub link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,12 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+        <img src={logo} alt='sumz_logo' className='w-28 object-contain hover:scale-105 transition-transform duration-300' />
 
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/Harshitt23/Online-Voting-System", "_blank")
+            window.open("https://github.com/Harshitt23/Ai-Summarizer", "_blank")
           }
           className='black_btn'
         >
@@ -21,7 +21,7 @@ const Hero = () => {
 
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>OpenAI GPT-4</span>
+        <span className='modern_gradient'>OpenAI GPT-4</span>
       </h1>
       <h2 className='desc'>
         Simplify your reading with Summize, an open-source article summarizer

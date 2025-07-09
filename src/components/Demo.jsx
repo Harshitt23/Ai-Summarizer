d242cf9de720d06c60f27146c382951210f0b7b0
Coding-master <harshitr2308@gmail.com>
2025-07-10 00:49:43 +0530

    feat: modernize UI, update fonts, gradients, and GitHub link

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -62,7 +62,7 @@ const Demo = () => {
   return (
     <section className='mt-16 w-full max-w-xl'>
       {/* Search */}
-      <div className='flex flex-col w-full gap-2'>
+      <div className='flex flex-col w-full gap-4'>
         <form
           className='relative flex justify-center items-center'
           onSubmit={handleSubmit}
@@ -70,7 +70,7 @@ const Demo = () => {
           <img
             src={linkIcon}
             alt='link-icon'
-            className='absolute left-0 my-2 ml-3 w-5'
+            className='absolute left-0 my-2 ml-3 w-5 opacity-60'
           />
 
           <input
@@ -84,14 +84,14 @@ const Demo = () => {
           />
           <button
             type='submit'
-            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+            className='submit_btn peer-focus:border-primary-500 peer-focus:text-primary-600'
           >
-            <p>↵</p>
+            <p className='text-lg'>↵</p>
           </button>
         </form>
 
         {/* Browse History */}
-        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+        <div className='flex flex-col gap-2 max-h-60 overflow-y-auto'>
           {allArticles.reverse().map((item, index) => (
             <div
               key={`link-${index}`}
@@ -105,7 +105,7 @@ const Demo = () => {
                   className='w-[40%] h-[40%] object-contain'
                 />
               </div>
-              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+              <p className='flex-1 font-inter text-primary-700 font-medium text-sm truncate'>
                 {item.url}
               </p>
             </div>
@@ -116,23 +116,28 @@ const Demo = () => {
       {/* Display Result */}
       <div className='my-10 max-w-full flex justify-center items-center'>
         {isFetching ? (
-          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+          <div className='flex flex-col items-center gap-4'>
+            <img src={loader} alt='loader' className='w-20 h-20 object-contain animate-pulse-slow' />
+            <p className='font-inter text-gray-600 font-medium'>Processing your article...</p>
+          </div>
         ) : error ? (
-          <p className='font-inter font-bold text-black text-center'>
-            Well, that wasn't supposed to happen...
-            <br />
-            <span className='font-satoshi font-normal text-gray-700'>
-              {error?.data?.error}
-            </span>
-          </p>
+          <div className='modern_card'>
+            <p className='font-inter font-bold text-black text-center'>
+              Well, that wasn't supposed to happen...
+              <br />
+              <span className='font-inter font-normal text-gray-700'>
+                {error?.data?.error}
+              </span>
+            </p>
+          </div>
         ) : (
           article.summary && (
-            <div className='flex flex-col gap-3'>
-              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
-                Article <span className='blue_gradient'>Summary</span>
+            <div className='flex flex-col gap-4 animate-fade-in'>
+              <h2 className='font-playfair font-bold text-gray-800 text-2xl text-center'>
+                Article <span className='text_gradient'>Summary</span>
               </h2>
               <div className='summary_box'>
-                <p className='font-inter font-medium text-sm text-gray-700'>
+                <p className='font-inter font-medium text-gray-800 leading-relaxed'>
                   {article.summary}
                 </p>
               </div>
